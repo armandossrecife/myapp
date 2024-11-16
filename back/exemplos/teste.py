50d4c552286f1c5dba861d50b0a9ad223c25c20c
armandossrecife <armando.sousa@gmail.com>
2024-11-16 11:56:08 -0300

    Novos testes com a feature de upload e download de imagens

--- back/exemplos/teste.py
+++ back/exemplos/teste.py
@@ -1,7 +1,11 @@
-from fastapi import FastAPI, Body, Header
+from fastapi import FastAPI, Body, Header, UploadFile, File, HTTPException
 import uvicorn
 import models
 from datetime import datetime
+import utilidades
+import uuid
+import os
+from fastapi.responses import FileResponse
 
 app = FastAPI()
 
@@ -42,7 +46,34 @@ def criar_tag(tag_in:models.TagIn = Body(embed=True)):
 
 @app.get('/tags')
 def lista_tags():
-    return minhas_tags
+    return "minhas listas ...."
+
+@app.post("/upload")
+async def upload_file(file: UploadFile = File(...)):
+    extensao = utilidades.get_file_extension(file.filename)
+    filename_uuid = str(uuid.uuid4())
+    uploaded_file = filename_uuid + "." + extensao
+    if utilidades.allowed_file(uploaded_file):
+        file_location = f"{utilidades.UPLOAD_DIRECTORY}/{uploaded_file}"
+        extensao = utilidades.get_file_extension(uploaded_file)
+        file_type = utilidades.get_media_type(extensao)
+        with open(file_location, "wb+") as file_object:
+            file_object.write(await file.read())
+    else:
+        raise HTTPException(status_code=404, detail="Erro: Tipo de arquivo não permitido")
+    return {"filename": file.filename, "type":file_type}
+
+@app.get("/files")
+def list_files():
+    files = os.listdir(utilidades.UPLOAD_DIRECTORY)
+    return {"files": files}
+
+@app.get("/download/{file_name}")
+def download_file(file_name: str):
+    file_path = f"{utilidades.UPLOAD_DIRECTORY}/{file_name}"
+    if not os.path.exists(file_path):
+        raise HTTPException(status_code=404, detail="File not found")
+    return FileResponse(file_path)
 
 if __name__ == "__main__": 
     uvicorn.run(debug=True)
